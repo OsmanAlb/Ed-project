5afec257d58bfb8374a3c384ceb75b3f04af011c
Magomed Elmurzaev <ban0le4@gmail.com>
2024-01-01 14:09:20 +0300

    conneted homePage and profilePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-import ProfilePage from './pages/ProfilePage';
-import HomePage from './pages/HomePage';
-import './App.css';
-
+import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage";
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
 function App() {
   return (
-    <div className="min-h-screen min-w-[375px] flex justify-center">
-      {/* <ProfilePage /> */}
-      <HomePage />
+    // min-h-screen
+    <div className="wrapper min-w-[375px] ">
+      <div className="wrapper__inner">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/profilePage" element={<ProfilePage />} />
+        </Routes>
+      </div>
+      <Nav />
     </div>
   );
 }
