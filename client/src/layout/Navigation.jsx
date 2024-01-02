c27c984376d441a7a38115d387ddf3b6f82b63cf
Eduard <>
2024-01-02 22:53:07 +0100

    add Navigation, add UseScrollVisibility

---
+++ client/src/layout/Navigation.jsx
@@ -0,0 +1,82 @@
+import {
+  HomeIconActive,
+  CourseIconActive,
+  NavProfileIconActive,
+  CourseIcon,
+  HomeIcon,
+  NavProfileIcon,
+} from '../components/ui/icons';
+import { classNames } from '../utils/classNames';
+import { useScrollVisibility } from '../hooks/useScrollVisibility';
+import { Link, useLocation } from 'react-router-dom';
+
+export const Navigation = () => {
+  const isVisible = useScrollVisibility();
+  const location = useLocation();
+  const currentRoute = location.pathname;
+
+  const isHome = currentRoute === '/';
+  const isCourses = currentRoute === '/courses';
+  const isProfile = currentRoute === '/profile';
+
+  return (
+    <nav
+      className={classNames(
+        'sticky flex justify-between items-center gap-[16px] bottom-0 h-[68px] w-[375px] mx-auto px-[16px] bg-background border-t-2 border-gray-200/50 z-10 transition',
+        isVisible ? 'translate-y-0' : 'translate-y-full'
+      )}
+    >
+      <Link
+        to="/"
+        className={classNames(
+          'w-[93px] h-[38px] flex justify-center items-center transition-transform',
+          isHome ? 'text-accent translate-x-0' : 'translate-x-8'
+        )}
+      >
+        {isHome ? <HomeIconActive /> : <HomeIcon />}
+        <span
+          className={classNames(
+            'ml-[5px] transition-all',
+            isHome ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-8'
+          )}
+        >
+          Home
+        </span>
+      </Link>
+      <Link
+        to="/courses"
+        className={classNames(
+          'w-[93px] h-[38px] flex justify-center items-center',
+          isCourses ? 'text-accent translate-x-0' : 'translate-x-5'
+        )}
+      >
+        {isCourses ? <CourseIconActive /> : <CourseIcon />}
+        <span
+          className={classNames(
+            'ml-[5px] transition-all',
+            isCourses ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-5'
+          )}
+        >
+          Courses
+        </span>
+      </Link>
+      <Link
+        to="/profile"
+        className={classNames(
+          'w-[93px] h-[38px] flex justify-center items-center',
+          isProfile ? 'text-accent translate-x-0' : 'translate-x-8'
+        )}
+      >
+        {isProfile ? <NavProfileIconActive /> : <NavProfileIcon />}
+        <span
+          className={classNames(
+            'ml-[5px] transition-all',
+            isProfile ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-8'
+          )}
+        >
+          Profile
+        </span>
+      </Link>
+    </nav>
+  );
+};
