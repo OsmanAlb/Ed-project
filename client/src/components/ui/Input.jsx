b805200fa99307caa7cdb903977fdd7185608a76
ANNA <89228060160q@gmail.com>
2024-01-06 18:17:53 +0300

    made input on register/search

--- client/src/components/ui/Input.jsx
+++ client/src/components/ui/Input.jsx
@@ -1,13 +1,49 @@
 
+import { classNames } from '../../utils/classNames';
 import { SearchIcon } from './icons/SearchIcon';
 
-const Input = (props) => {
+export const Input = ({
+	className,
+	link,
+	variant = "search",
+	...props
+}) => {
+
+	const divInputClasses = classNames(
+		"flex gap-3 mb-6",
+		variant === "search"
+			? 'bg-[#F2F2F2] border border-[#E0E0E0] rounded-md p-[15px]'
+			: '',
+		variant === 'register'
+			? 'border-b border-[#9D9B9B] bg-inherit p-[10px]'
+			: '',
+		className
+	)
+
+	const inputClasses = classNames(
+		'bg-inherit outline-none',
+		variant === "search"
+			? 'text-12px leading-[22px] text-[#4F4F4F]'
+			: '',
+		variant === 'register'
+			? 'text-14px leading-[24px] text-[#181818]'
+			: '',
+		className
+	);
+
+	if (link) {
+		return (
+			<Link to={link} className={inputClasses} {...props}>
+				{children}
+			</Link>
+		);
+	}
+
 	return (
-		<div className="flex gap-3 bg-[#F2F2F2] border border-[#E0E0E0] p-[15px] mb-6 rounded-md">
-			<SearchIcon />
+		<div className={divInputClasses}>
+			{variant === "search" && <SearchIcon />}
 			<input
-				placeholder="Search"
-				className="text-12px leading-[22px] bg-inherit outline-none text-[#4F4F4F]" {...props}
+				className={inputClasses} {...props}
 			/>
 		</div>
 	)
