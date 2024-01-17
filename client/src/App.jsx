75398cc292c012bc3589f2da893eeb9b8dcaccbd
Osman Alb <ernestodelogruz@gmail.com>
2024-01-17 13:46:27 +0300

    Resolve conflict 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,27 @@
-import ProfilePage from './pages/ProfilePage';
-import HomePage from './pages/HomePage';
-import { Routes, Route } from 'react-router-dom';
-import OnBoardingPage from './pages/OnBoardingPage';
-import { Layout } from './layout/Layout';
-import NotFoundPage from './pages/NotFoundPage';
-import QuizPage from './pages/QuizPage';
+import ProfilePage from './pages/ProfilePage'
+import HomePage from './pages/HomePage'
+import { Routes, Route } from 'react-router-dom'
+import OnBoardingPage from './pages/OnBoardingPage'
+import { Layout } from './layout/Layout'
+import NotFoundPage from './pages/NotFoundPage'
+import QuizPage from './pages/QuizPage'
 
 function App() {
-  return (
-    <>
-      <div className='min-h-screen min-w-[375px] max-w-screen sm:max-w-[375px] sm:mx-auto flex flex-col bg-background overflow-clip'>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/quiz" element={<QuizPage />} />
-          </Route>
-          <Route path='/on-boarding' element={<OnBoardingPage />} />
-          <Route path='*' element={<NotFoundPage />} />
-
-        </Routes>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className='min-h-screen min-w-[375px] max-w-screen sm:max-w-[375px] sm:mx-auto flex flex-col bg-background overflow-clip'>
+				<Routes>
+					<Route element={<Layout />}>
+						<Route path='/' element={<HomePage />} />
+						<Route path='/profile' element={<ProfilePage />} />
+						<Route path='/quiz' element={<QuizPage />} />
+					</Route>
+					<Route path='/on-boarding' element={<OnBoardingPage />} />
+					<Route path='*' element={<NotFoundPage />} />
+				</Routes>
+			</div>
+		</>
+	)
 }
 
 export default App
