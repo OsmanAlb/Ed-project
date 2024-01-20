15d14d26cf8603be452411342b1df2e2f035aa2d
ANNA <89228060160q@gmail.com>
2024-01-20 19:51:28 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import { Layout } from './layout/Layout'
+import CreateCourses from './pages/CreateCourses'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 import NotFoundPage from './pages/NotFoundPage'
 import OnBoardingPage from './pages/OnBoardingPage'
 import ProfilePage from './pages/ProfilePage'
 import QuizPage from './pages/QuizPage'
 import { RegistrationPage } from './pages/RegistrationPage'
-import LoginPage from './pages/LoginPage'
 
 function App() {
 	return (
@@ -17,6 +18,7 @@ function App() {
 						<Route path='/' element={<HomePage />} />
 						<Route path='/profile' element={<ProfilePage />} />
 						<Route path='/quiz' element={<QuizPage />} />
+						<Route path='/create' element={<CreateCourses />} />
 					</Route>
 					<Route
 						path='/registration'
