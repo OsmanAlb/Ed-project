895352a7d858e35480f30dbfd10866bc304fa94d
Osman Alb <ernestodelogruz@gmail.com>
2024-02-02 14:31:56 +0300

    Resolve conflict with read.me file

---
+++ client/src/pages/AddPost/index.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import SimpleMDE from "react-simplemde-editor";
+import { selectorIsAuth } from "../../redux/slices/auth";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import axios  from "../../axios";
+
+import "easymde/dist/easymde.min.css";
+import styles from "./AddPost.module.scss";
+
+export const AddPost = () => {
+  const isAuth = useSelector(selectorIsAuth);
+  const [isLoading, setLoading] = React.useState(false)
+  const [value, setValue] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
+  const [tags, setTags] = React.useState("");
+  const inputFileRef = React.useRef(null);
+
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append("image", file);
+      const { data } = await axios.post("/upload", formData);
+      console.log(data);
+      setImageUrl(data.url)
+    } catch (error) {
+      console.warn(error);
+      alert(`Error uploading file: ${error.message}`);
+    }
+  };
+
+  const onClickRemoveImage = () => {
+    setImageUrl('')
+  };
+
+  const onChange = React.useCallback((value) => {
+    setValue(value);
+  }, []);
+
+  const options = React.useMemo(
+    () => ({
+      spellChecker: false,
+      maxHeight: "400px",
+      autofocus: true,
+      placeholder: "Введите текст...",
+      status: false,
+      autosave: {
+        enabled: true,
+        delay: 1000,
+      },
+    }),
+    []
+  );
+
+  if (!window.localStorage.getItem("token") && !isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Paper style={{ padding: 30 }}>
+      <Button
+        onClick={() => {
+          inputFileRef.current.click();
+        }}
+        variant="outlined"
+        size="large"
+      >
+        Загрузить превью
+      </Button>
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
+      {imageUrl && (
+        <>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Удалить
+          </Button>
+          <img
+            className={styles.image}
+            src={`http://localhost:4444${imageUrl}`}
+            alt="Uploaded"
+          />
+        </>
+      )}
+      <br />
+      <br />
+      <TextField
+        classes={{ root: styles.title }}
+        variant="standard"
+        placeholder="Название темы..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+      />
+      <TextField
+        classes={{ root: styles.tags }}
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+        variant="standard"
+        placeholder="раздел"
+        fullWidth
+      />
+      <SimpleMDE
+        className={styles.editor}
+        value={value}
+        onChange={onChange}
+        options={options}
+      />
+      <div className={styles.buttons}>
+        <Button size="large" variant="contained">
+          Опубликовать
+        </Button>
+        <a href="/">
+          <Button size="large">Отмена</Button>
+        </a>
+      </div>
+    </Paper>
+  );
+};
